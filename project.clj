4bbb80a3f3ccdfde812d34e812a1a5bed6c27141
Huw Giddens <huw@robotines.co.nz>
2011-03-31 13:30:07 +1300

    Replace placeholder dependencies in project.clj with real ones.

--- project.clj
+++ project.clj
@@ -4,9 +4,8 @@
                  [org.clojure/clojure-contrib "1.2.0"]
                  [org.samba.jcifs/jcifs "1.2.19"]
                  [org.apache.poi/poi "3.7"]
-                 ;; ical4j
-                 ;; compojure
-                 ;; ring-jetty-adapter
-                 ;; hiccup
-                 ]
+                 [ical4j "0.9.20"]
+                 [compojure "0.6.2"]
+                 [ring/ring-jetty-adapter "0.3.7"]
+                 [hiccup "0.3.4"]]
   :dev-dependencies [[swank-clojure "1.2.1"]])
